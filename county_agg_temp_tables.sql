88fbcf72050fe356a2d71e18c392af3a94dc96e1
David Roell <breeroell@gmail.com>
2016-05-12 18:08:38 -0400

    prototype boom bust recovery pattern

--- county_agg_temp_tables.sql
+++ county_agg_temp_tables.sql
@@ -1,5 +1,5 @@
-﻿ CREATE TEMPORARY TABLE
-  county_apps AS (
+﻿ CREATE TABLE
+  county_apps_2002 AS (
   SELECT year, state, county,
 	ROUND(avg(amount::INTEGER),2) AS loan_average_app,
         ROUND(avg(income::INTEGER),2) AS income_average_app,
@@ -8,7 +8,7 @@
         --ROUND(((amount::NUMERIC / 0.80) / income::NUMERIC),2) AS income_mult_app,
 	CONCAT(state, county) AS fips
 
-        FROM hmdalar2000
+        FROM hmdalar2002
 
         WHERE loan_type = '1'
         AND loan_purpose in ('1', '3')
@@ -19,27 +19,28 @@
 
         GROUP BY state, county, year, amount, income);
 
- Select * from county_apps;
+commit;
 
- CREATE TEMPORARY TABLE
- county_orig AS (
- SELECT year, state, county,
-        ROUND(avg(amount::INTEGER),2) AS loan_average_orig,
-        ROUND(avg(income::INTEGER),2) AS income_average_orig,
-        COUNT(concat(agency, rid)) AS count_orig,
-        SUM(amount::INTEGER) AS value_orig,
-        --ROUND(((amount::NUMERIC / 0.80) / income::NUMERIC),2) AS income_mult_orig,
-        CONCAT(state, county) AS fips
+ CREATE TABLE
+  county_orig_2001 AS (
+  SELECT year, state, county,
+	ROUND(avg(amount::INTEGER),2) AS loan_average_app,
+        ROUND(avg(income::INTEGER),2) AS income_average_app,
+        COUNT(concat(agency, rid)) AS count_app,
+        SUM(amount::INTEGER) AS value_app,
+        --ROUND(((amount::NUMERIC / 0.80) / income::NUMERIC),2) AS income_mult_app,
+	CONCAT(state, county) AS fips
 
-        FROM hmdalar2000
+        FROM hmdalar2001
 
         WHERE loan_type = '1'
-        AND action = '1'
         AND loan_purpose in ('1', '3')
+        AND action = '1'
         AND income NOT LIKE '%NA%'
         AND income NOT LIKE '%na% '
         AND amount NOT LIKE '%NA%'
         AND amount NOT LIKE '%na%'
 
-        GROUP BY state, county, year, amount, income)
+        GROUP BY state, county, year, amount, income); 
 
+        commit;
