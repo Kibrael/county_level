1fd279ae4fda376dbb253fb9e430d5e9be74972a
David Roell <breeroell@gmail.com>
2016-06-19 19:42:44 -0400

    finish demographic deltas

--- split_by_county.py
+++ split_by_county.py
@@ -12,7 +12,7 @@ conn = psycopg2.connect("dbname=hmdamaster user=roellk") #connect and return con
 cur = conn.cursor() #establish SQL cursor object
 
 #load tract to CBSA file to get valid counties
-cbsa_df = get_CBSA_df('tract_to_cbsa_2010.csv', '|')
+cbsa_df = get_CBSA_df('tract_to_cbsa_2010.csv', '|') #loads CBSA data and left pads 0's
 fips_list = set(cbsa_df.county.ravel()) #remove duplicates
 
 ##########################
@@ -27,14 +27,17 @@ def income_multiple(df=None, race_name=None, action='app', numerator=None, denom
 	if race_name is None:
 		col_name = 'income_multiple_' + action
 	else:
-		col_name = race_name + '_' + income_multiple + action
+		col_name = race_name + '_' + 'income_multiple_' + action
 	df[col_name] = (df[numerator] / 0.80) / df[denominator]
 
 
 def percent_change(df=None, col_list=None):
-	for col in col_list
+	for col in col_list:
 		out_col = col + '_delta'
-		df[out_col] = df[col].pct_change() * 100
+		try:
+			df[out_col] = df[col].pct_change() * 100
+		except:
+			print("unable to compute all values for {column}".format(column=col))
 
 #select data for 2000-2014
 for fips in list(set(fips_list)):
@@ -65,23 +68,24 @@ for fips in list(set(fips_list)):
 		income_multiple(df=out_file, action='orig', numerator='loan_average_orig', denominator='income_average_orig')
 
 		#create deltas for pattern building
-		delta_cols = ['loan_avg_app', 'income_average_app', 'count_app', 'value_app', 'income_mult']
-		percent_change(df=out_file, col_list=delta_cols)
-		#out_file['loan_avg_app_delta'] = out_file.loan_average_app.pct_change()*100
-		#out_file['income_average_app_delta'] = out_file.income_average_app.pct_change() *100
-		#out_file['count_app_delta'] = out_file.count_app.pct_change() *100
-		#out_file['value_app_delta'] = out_file.value_app.pct_change() * 100
-		#out_file['income_mult_delta'] = out_file.income_multiple_app.pct_change()*100
+		app_delta_cols = ['loan_average_app', 'income_average_app', 'count_app', 'value_app', 'income_multiple_app']
+		orig_delta_cols = ['loan_average_orig', 'income_average_orig', 'count_orig', 'value_orig', 'income_multiple_orig']
+		percent_change(df=out_file, col_list=app_delta_cols)
+		percent_change(df=out_file, col_list=orig_delta_cols)
+
 		for race in race_dict.keys():
-			numerator = race_dict[race] + '_loan_average_'
-			denominator = race_dict[race] + '_income_average_'
-			income_multiple(df=out_file, action='app', numerator=numerator + 'app', denominator=denominator+'app')
-			income_multiple(df=out_file, action='orig', numerator=numerator + 'orig', denominator=denominator+'orig')
-		#FIXME add demographic deltas here
+			numerator_text = race_dict[race] + '_loan_average_'
+			denominator_text = race_dict[race] + '_income_average_'
+			income_multiple(df=out_file, race_name=race_dict[race], action='app', numerator=numerator_text + 'app', denominator=denominator_text+'app')
+			income_multiple(df=out_file, race_name=race_dict[race], action='orig', numerator=numerator_text + 'orig', denominator=denominator_text+'orig')
+
+			race_app_delta_cols = [race_dict[race] + '_' + col for col in app_delta_cols]
+			race_orig_delta_cols = [race_dict[race] + '_' + col for col in orig_delta_cols]
+			percent_change(df=out_file, col_list=race_app_delta_cols)
+			percent_change(df=out_file, col_list=race_orig_delta_cols)
+
 	if not os.path.exists(path): #check to see if path for a county exists
 			os.makedirs(path) #create path if it is not present
 	print('writing data for {path}'.format(path=path))
-	for col in out_file.columns.ravel():
-		print col
 	out_file.to_csv(path+"data.csv", index=None) #write dataframe to CSV for one county in state/county/file directory structure
 
